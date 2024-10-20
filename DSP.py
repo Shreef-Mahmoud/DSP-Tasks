a91f87f73338012cdfb6a92b2ef8e8ce31981084
Seif Hossam <saifhossammohamed4@gmail.com>
2024-10-21 01:15:04 +0300

    modified dsp

--- DSP.py
+++ DSP.py
@@ -9,36 +9,36 @@ import matplotlib.pyplot as plt
 
 def SignalSamplesAreEqual(indices,samples):
     file_name = filedialog.askopenfilename(title="Choose The Compare File", filetypes=(("text files", ".txt"), ("all files", ".*")))
-    expected_indices=[]
-    expected_samples=[]
+    expected_indices = []
+    expected_samples = []
     with open(file_name, 'r') as f:
         line = f.readline()
         line = f.readline()
         line = f.readline()
         line = f.readline()
         while line:
-            # process line
-            L=line.strip()
-            if len(L.split(' '))==2:
-                L=line.split(' ')
-                V1=int(L[0])
-                V2=float(L[1])
+
+            L = line.strip()
+            if len(L.split(' ')) == 2:
+                L = line.split(' ')
+                V1 = int(L[0])
+                V2 = float(L[1])
                 expected_indices.append(V1)
                 expected_samples.append(V2)
                 line = f.readline()
             else:
                 break
-                
-    if len(expected_samples)!=len(samples):
-        print("Test case failed, your signal have different length from the expected one")
+
+    if len(expected_samples) != len(samples):
+        mb.showerror("Test Case Failed", "Your signal has a different length from the expected one.")
         return
     for i in range(len(expected_samples)):
         if abs(samples[i] - expected_samples[i]) < 0.01:
             continue
         else:
-            print("Test case failed, your signal have different values from the expected one") 
+            mb.showerror("Test Case Failed", "Your signal has different values from the expected one.")
             return
-    print("Test case passed successfully")
+    mb.showinfo("Test Case Passed", "Test case passed successfully.")
 
 
 
@@ -78,14 +78,14 @@ def generateSignal():
     samplingFrequency = int(mytext3.get())
     phaseShift = float(mytext4.get())
 
-    if(samplingFrequency < (2 *analogfrequency)  ):
-        mb.showerror(title = "Sampling Theory", message = "Wrong Sampling Frequency\nhint : Sampling Frequency must be greater than 2 * Frequency")
+    if samplingFrequency < (2 *analogfrequency):
+        mb.showerror(title="Sampling Theory", message="Wrong Sampling Frequency\nhint : Sampling Frequency must be greater than 2 * Frequency")
         return
 
     digitalfrequency = analogfrequency / samplingFrequency
 
-    if (mycombo1.get() == "Sin"):
-        for i in range(0 ,samplingFrequency):
+    if mycombo1.get() == "Sin":
+        for i in range(0, samplingFrequency):
             firstValue = 2 * math.pi * digitalfrequency * i
             insideSin = firstValue + phaseShift
             sinValue = math.sin(insideSin)
@@ -94,8 +94,8 @@ def generateSignal():
             indices.append(i)
             samples.append(round(pointAmplitude, 3))
 
-    elif (mycombo1.get() == "Cos"):
-        for i in range(0 ,samplingFrequency):
+    elif mycombo1.get() == "Cos":
+        for i in range(0, samplingFrequency):
             firstValue = 2 * math.pi * digitalfrequency * i
             insideSin = firstValue + phaseShift
             sinValue = math.cos(insideSin)
@@ -103,25 +103,44 @@ def generateSignal():
 
             indices.append(i)
             samples.append(round(pointAmplitude, 3))
-    else :
-        mb.showerror(title = "Signal Type Error", message = "Select A Signal Type (Sin or Cos)")
+    else:
+        mb.showerror(title="Signal Type Error", message="Select A Signal Type (Sin or Cos)")
         return
-    plotingSignal(indices ,samples , samplingFrequency)
+    plotingSignal(indices, samples, samplingFrequency)
 
 def readFileDirct():
-    if(operations.get() == "None"):
+    if operations.get() == "None":
         openFile()
-    else :
+    else:
         mathOperation()
 
-def mathOperation ():
-    if(operations.get() == "Add"):
-        filepath1 = openFile()
-        signal1_indices , signal1_samples = readfile(filepath1)
-        filepath2 = openFile()
-        signal2_indices , signal2_samples = readfile(filepath2)
 
-        if(len(signal1_indices) == len(signal2_indices)):
+def normalize_signal(signal, range_type='-1 to 1'):
+
+    min_val = np.min(signal)
+    max_val = np.max(signal)
+
+    if range_type == '-1 to 1':
+
+        normalized_signal = 2 * (signal - min_val) / (max_val - min_val) - 1
+    elif range_type == '0 to 1':
+
+        normalized_signal = (signal - min_val) / (max_val - min_val)
+    else:
+        raise ValueError("range_type must be either '-1 to 1' or '0 to 1'.")
+
+    return normalized_signal
+
+
+def mathOperation ():
+
+    if operations.get()=="Add":
+        filepath1 = openFile()
+        signal1_indices, signal1_samples = readfile(filepath1)
+        filepath2 = openFile()
+        signal2_indices, signal2_samples = readfile(filepath2)
+
+        if len(signal1_indices) == len(signal2_indices):
             arr1_samples = np.array(signal1_samples)
             arr2_samples = np.array(signal2_samples)
 
@@ -129,13 +148,13 @@ def mathOperation ():
 
             indices = signal1_indices
             samples = arr_Of_Samples.tolist()
-            plotingSignal(indices,samples, len(indices) )
+            plotingSignal(indices, samples, len(indices))
 
-    elif(operations.get() == "Subtract"):
+    elif operations.get() == "Subtract":
         filepath1 = openFile()
-        signal1_indices , signal1_samples = readfile(filepath1)
+        signal1_indices, signal1_samples = readfile(filepath1)
         filepath2 = openFile()
-        signal2_indices , signal2_samples = readfile(filepath2)
+        signal2_indices, signal2_samples = readfile(filepath2)
 
         if(len(signal1_indices) == len(signal2_indices)):
             arr1_samples = np.array(signal1_samples)
@@ -145,11 +164,11 @@ def mathOperation ():
 
             indices = signal1_indices
             samples = arr_Of_Samples.tolist()
-            plotingSignal(indices,samples, len(indices) )
+            plotingSignal(indices, samples, len(indices))
 
-    elif(operations.get() == "Multiply"):
+    elif operations.get() == "Multiply":
         filepath1 = openFile()
-        signal1_indices , signal1_samples = readfile(filepath1)
+        signal1_indices, signal1_samples = readfile(filepath1)
 
         mutlipleVarabile = int(multi.get())
         arr1_samples = np.array(signal1_samples)
@@ -158,23 +177,23 @@ def mathOperation ():
 
         indices = signal1_indices
         samples = arr1_samples.tolist()
-        plotingSignal(indices,samples, len(indices) )
+        plotingSignal(indices, samples, len(indices))
 
-    elif(operations.get() == "Square"):
+    elif operations.get() == "Square":
         filepath1 = openFile()
-        signal1_indices , signal1_samples = readfile(filepath1)
+        signal1_indices, signal1_samples = readfile(filepath1)
 
         arr1_samples = np.array(signal1_samples)
 
-        arr_Of_Samples = pow(arr1_samples ,2)
+        arr_Of_Samples = pow(arr1_samples, 2)
 
         indices = signal1_indices
         samples = arr_Of_Samples.tolist()
-        plotingSignal(indices,samples, len(indices) )
+        plotingSignal(indices, samples, len(indices))
 
-    elif(operations.get() == "Accumulate"): 
+    elif operations.get() == "Accumulate":
         filepath1 = openFile()
-        signal1_indices , signal1_samples = readfile(filepath1)
+        signal1_indices, signal1_samples = readfile(filepath1)
 
         signal1_samples = []
         temp = 0
@@ -182,8 +201,29 @@ def mathOperation ():
             temp += n
             signal1_samples.append(temp)
 
-        plotingSignal(signal1_indices,signal1_samples, len(signal1_indices) )
+        plotingSignal(signal1_indices, signal1_samples, len(signal1_indices))
 
+
+    elif operations.get() == "Normalize":
+
+        filepath1 = openFile()
+
+        signal1_indices, signal1_samples = readfile(filepath1)
+
+
+        range_type = mb.askquestion("Normalization", "Normalize to '0 to 1' or '-1 to 1'? (default is '0 to 1')")
+
+        range_type = '0 to 1' if range_type == 'yes' else '-1 to 1'
+
+        arr1_samples = np.array(signal1_samples)
+
+        normalized_samples = normalize_signal(arr1_samples, range_type)
+
+        indices = signal1_indices
+
+        samples = normalized_samples.tolist()
+
+        plotingSignal(indices, samples, len(indices))
 
 def plotingSignal(indices, samples, samplingFrequency):
     indicesArr = np.array(indices)
@@ -207,14 +247,14 @@ def plotingSignal(indices, samples, samplingFrequency):
 
     plt.show()
 
-    SignalSamplesAreEqual(indices , samples)
+    SignalSamplesAreEqual(indices, samples)
 
 
 myframe = Tk()
 myframe.title("DSP")
-myframe.geometry("600x600")
+myframe.geometry("1800x900")
 
-p1 = PhotoImage(file="C:/DSP/icon.png")
+p1 = PhotoImage(file="C:/DSP/DSP-Tasks/icon.png")
 myframe.iconphoto(False, p1)
 
 mylabel =ttk.Label(myframe, text="Select Signal", font="Calibre 20 bold")
@@ -224,45 +264,48 @@ mycombo1 = ttk.Combobox(myframe, values=["Sin", "Cos"], width=47)
 mycombo1.place(relx=0.5, rely=0.5, y=-200, anchor="center")
 mycombo1.current(0)
 
-operations = ttk.Combobox(myframe, values=["None", "Add" , "Subtract" , "Multiply" , "Square" , "Normarlize" , "Accumulate"], width=47)
-operations.place(relx=0.5, rely=0.5, y=-150, anchor="center")
+label =ttk.Label(myframe, text="Arithmetic Operations", font="Calibre 20 bold")
+label.place(relx=0.5, rely=0.5, x=400, y=-250, anchor="center")
+
+operations = ttk.Combobox(myframe, values=["None", "Add", "Subtract", "Multiply", "Square", "Normalize", "Accumulate"], width=47)
+operations.place(relx=0.5, rely=0.5, x=400, y=-200, anchor="center")
 operations.current(0)
 
 mylab = ttk.Label(myframe, text="Amplitude")
-mylab.place(relx=0.5, rely=0.5, y=-105, anchor="center")
+mylab.place(relx=0.5, rely=0.5, x=-450, y=-105, anchor="center")
 
 mytext1 = ttk.Entry(myframe, width=50)
-mytext1.place(relx=0.5, rely=0.5, y=-80, anchor="center")
+mytext1.place(relx=0.5, rely=0.5, x=-450, y=-80, anchor="center")
 
 mylab = ttk.Label(myframe, text="frequency in radians per second")
-mylab.place(relx=0.5, rely=0.5, y=-45, anchor="center")
+mylab.place(relx=0.5, rely=0.5, x=-450, y=-45, anchor="center")
 
 mytext2 = ttk.Entry(myframe, width=50)
-mytext2.place(relx=0.5, rely=0.5,y=-20, anchor="center")
+mytext2.place(relx=0.5, rely=0.5, x=-450, y=-20, anchor="center")
 
 mylab = ttk.Label(myframe, text="sampling frequency")
-mylab.place(relx=0.5, rely=0.5, y=15, anchor="center")
+mylab.place(relx=0.5, rely=0.5, x=-450, y=15, anchor="center")
 
 mytext3 = ttk.Entry(myframe, width=50)
-mytext3.place(relx=0.5, rely=0.5, y=40, anchor="center")
+mytext3.place(relx=0.5, rely=0.5, x=-450, y=40, anchor="center")
 
 mylab = ttk.Label(myframe, text="phase in radians")
-mylab.place(relx=0.5, rely=0.5, y=75, anchor="center")
+mylab.place(relx=0.5, rely=0.5, x=-450, y=75, anchor="center")
 
 mytext4 = ttk.Entry(myframe, width=50)
-mytext4.place(relx=0.5, rely=0.5, y=100, anchor="center")
+mytext4.place(relx=0.5, rely=0.5, x=-450, y=100, anchor="center")
 
 multilab = ttk.Label(myframe, text="Multiple Value")
-multilab.place(relx=0.5, rely=0.5, y=125, anchor="center")
+multilab.place(relx=0.5, rely=0.5, x=-450, y=135, anchor="center")
 
 multi = ttk.Entry(myframe, width=50)
-multi.place(relx=0.5, rely=0.5, y=160, anchor="center")
+multi.place(relx=0.5, rely=0.5, x=-450, y=160, anchor="center")
 
 mybottom =ttk.Button(myframe,text="Display Result",width=50 , command=generateSignal)
-mybottom.place(relx=0.5,rely=0.5,y=210,anchor="center")
+mybottom.place(relx=0.5, rely=0.5, y=210,anchor="center")
 
 mybottom2 = ttk.Button(myframe, text="Read File", width=50, command=readFileDirct)
-mybottom2.place(relx=0.5, rely=0.5, y=250, anchor="center")
+mybottom2.place(relx=0.5, rely=0.5, x=400, y=210, anchor="center")
 
 def move_1(event):
     mytext2.focus()
